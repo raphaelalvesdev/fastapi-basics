f077939c1859cdb9f2c2527da403addcea9aefa6
phael77 <rapha555lima@gmail.com>
2025-08-05 20:54:29 -0300

    feat: testing metrics from prometheus

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+from prometheus_client import make_asgi_app
+
 app = FastAPI()
 
 class Item(BaseModel):
@@ -21,3 +23,6 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
+metrics_app = make_asgi_app()
+app.mount("/metrics", metrics_app)
